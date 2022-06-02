d7f46ed8a47a71fd7881af32202de03cf6ac95a4
dhruval-moradiya-au18 <dhruvalmoradiya@gmail.com>
2022-06-02 15:56:21 +0530

    git demo

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,8 @@ const externalModule = require('../logger/logger.js')
 const externalDate = require('../util/helper.js')
 const externalhelper = require('../util/helper.js')
 const externalvalidator=require('../validator/formatter')
-
 const router = express.Router();
+const lodash = require('lodash')
 
 router.get('/test-me', function (req, res) {
     externalModule.welcome()
@@ -21,17 +21,29 @@ router.get('/test-me', function (req, res) {
 });
 
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
+router.get('/hello', function (req, res) {
+    let month = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    let subArray = lodash.chunk(month, 4)
+    console.log(subArray)
+    res.send('My second api!')
+
+    
+    let oddNumber = [1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(oddNumber))
+
+    
+    let a =[1,4,5,6]
+    let b =[2,5,2,9]
+    let c =[3,2,6,7,8]
+    let d =[5,5,5]
+    let e =[1,2,3,4,5,6,7,8,9,10]
+        console.log(lodash.union(a,b,c,d,e))
+
+        let keyValue=[["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+               console.log(lodash.fromPairs(keyValue))
 });
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
 
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
