3fe871ae4f7b74fa3b58b8d46440daf85b340759
dhruval-moradiya-au18 <dhruvalmoradiya@gmail.com>
2022-06-02 22:48:41 +0530

    git demo

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,11 +12,22 @@ router.get('/test-me', function (req, res) {
     console.log('The first element received from underscope function is '+firstElement)
     res.send('My first ever api!')
 });
+router.get("/missingNumber", function (req, res) {
+    let arr= [1,2,3,5,6,7]
+ 
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+ 
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
 
 router.get('/candidates', function(req, res){
     console.log('Query paramters for this request are '+JSON.stringify(req.query))
@@ -29,13 +40,45 @@ router.get('/candidates', function(req, res){
     let candidates = ['Akash','Suman']
     res.send(candidates)
 })
+router.get("/missingNumber2", function(req, res){
+let arr= [33, 34, 35, 37, 38]
+   let len= arr.length
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let firstDigit= arr[0]
+   let lastDigit= arr.pop()
+   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+   let missingNumber= consecutiveSum - total
+  
+   res.send(  { data: missingNumber  }  );
+});
 
 router.get('/candidates/:canidatesName', function(req, res){
+   
     console.log('The request objects is '+ JSON.stringify(req.params))
     console.log('Candidates name is '+req.params.canidatesName)
     res.send('Done')
 })
 
+router.get("/movies", function(req, res){
+    let movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]
+console.log(movies)
+    res.send(  { movies }  );
+});
+
+
+router.get("/movies/:indexNumber", function(req, res){
+    let movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]
+  
+let index = movies.findIndex()
+  
+console.log(index)
+    res.send(  { Done }  );
+});
 
 module.exports = router;
 // adding this comment for no reason
