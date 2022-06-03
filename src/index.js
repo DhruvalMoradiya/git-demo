8d55dcc552b873495b6890d5468acb966ec1b3a6
dhruval-moradiya-au18 <dhruvalmoradiya@gmail.com>
2022-06-04 00:27:07 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Dhruval74:74053451dm@@dhruval.8w4kvho.mongodb.net/Dhruval74?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
